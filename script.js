da9601d0a58cc8b499498f25e11f415e9eec8f2c
arshi <parveenarshi0116@gmail.com>
2025-02-05 12:02:11 +0530

    Add files via upload
    added script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 import { createRoot } from 'react-dom/client'
 import './style.css'
 
-function Card(key,title,img,decription) {
+function Card({title,image,description  }) {
     return (
     
-        <div className='card' key={key}>
-         
-        <img src= {img} alt="arshi" />
+        <div className='card' >
+        <img src= {image} alt="arshi" />
         <h3>{title}</h3>
-        <p>{decription}</p>
+        <p>{description }</p>
     </div>
         
     )
 
 }
 
-
 const root = createRoot(document.getElementById('root'))
 fetch('https://dummyjson.com/products')
 .then(res => res.json())
 .then((data)=>{
     console.log(data);
-    const container2=data.products.map(function (products) {
-
-        return Card(products.id, products.title, products.thumbnail,products.description,)
-    })
-    root.render(<div className='container'>{container2}</div>)
+    root.render(<div className='container'>{
+    data.products.map((product) =>(
+        <Card
+        key={product.id}
+        image={product.thumbnail}
+        title={product.title}
+        description ={product.description }/>)   
+    )}
+    </div>)
 })
+    
